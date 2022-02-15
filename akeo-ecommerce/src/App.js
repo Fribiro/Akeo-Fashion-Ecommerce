3bf43f001c5951397252821a20e38256c85008dd
Salma Mohamed <85572214+salmamoh905@users.noreply.github.com>
2022-02-15 07:00:42 -0500

    added the navbar and announcement

--- akeo-ecommerce/src/App.js
+++ akeo-ecommerce/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Hello</h1>
+    <div>
+      <Home/>
     </div>
+     
+    
   );
 }
 
---
+++ akeo-ecommerce/src/pages/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Announcement from '../components/navigation/Announcement'
+import Navbar from '../components/navigation/Navbar'
+
+const Home = () => {
+  return (
+    <div>
+      <Announcement></Announcement>
+      <Navbar></Navbar>
+    </div>
+  )
+}
+
+export default Home
