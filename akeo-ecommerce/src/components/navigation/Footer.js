e24b53d8ae8b1dcc58984cd51fc932fb7dba7caa
nivlapeter <nivlapeter.0549@gmail.com>
2022-02-15 13:57:32 +0300

    Ommit socials
    removed linked in social link

--- akeo-ecommerce/src/components/navigation/Footer.js
+++ akeo-ecommerce/src/components/navigation/Footer.js
@@ -170,7 +170,7 @@ const Footer = () => {
             <a href="#"><i class="fab fa-facebook-f"></i></a>
             <a href="#"><i class="fab fa-twitter"></i></a>
             <a href="#"><i class="fab fa-instagram"></i></a>
-            <a href="#"><i class="fab fa-linkedin-in"></i></a>
+            {/* <a href="#"><i class="fab fa-linkedin-in"></i></a> */}
 
           </div>
         </div>
